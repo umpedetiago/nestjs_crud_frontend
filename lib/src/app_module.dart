e38ddee7dd4a0083ea848b54845ce8ce0c1fec65
umpedetiago <tiagopinheirobg@gmail.com>
2023-08-04 12:03:15 -0300

    add Modular

---
+++ lib/src/app_module.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  @override
+  void binds(i) {
+    // TODO: implement binds
+    super.binds(i);
+  }
+
+  @override
+  void routes(r) {
+    // TODO: implement routes
+    super.routes(r);
+  }
+}
